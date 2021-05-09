c23d44c4875ca99e6763bc132705ebb8d737a1e8
Aniruddha Daryapurkar <daryapurkar.aniruddha@gmail.com>
2021-05-09 20:41:07 +0530

    Add header validator

--- lib/api/validator.py
+++ lib/api/validator.py
@@ -3,20 +3,15 @@ from dataclasses import asdict
 from testdata.datafactory import Model
 
 
-def validate_schema(response: Response, schema_model: Model) -> bool:
-    try:
-        response_type = schema_model.__class__
-        payload = response.json()
-        d = response_type.from_dict(payload)
-        return True
-    except:
-        return False
+def validate_schema(response: dict, schema_model: type) -> bool:
+    Model.parse(response, schema_model)
+    return True
 
 
 def validate_response_body(response: (Response, dict), expected_response_model: Model) -> bool:
     if isinstance(response, Response):
         response = response.json()
-    response_model = Model.parse(response, expected_response_model.__class__)
+    response_model = Model.parse(response, type(expected_response_model))
     return True if expected_response_model == response_model else False
 
 
@@ -28,33 +23,10 @@ def validate_response_body_as_json(response: (Response, dict), expected_response
     return True if expected_response_model == response else False
 
 
-# class Validator:
-#     """
-#     Validate schema
-#     Validate response status and body
-#     Validate headers
-#     """
-#
-#     def validate_schema(self, response: Response, schema_model: Model)-> bool:
-#         try:
-#             response_type = schema_model.__class__
-#             payload = response.json()
-#             d = response_type.from_dict(payload)
-#             return True
-#         except:
-#             return False
-#
-#     def validate_response_body(self, response: (Response, dict), expected_response_model: Model) -> bool:
-#         if isinstance(response, Response):
-#             response = response.json()
-#         response_model = Model.parse(response, expected_response_model.__class__)
-#         return True if expected_response_model == response_model else False
-#
-#     def validate_response_body_as_json(self, response: (Response, dict), expected_response_model: (Model, dict)) -> bool:
-#         if isinstance(response, Response):
-#             response = response.json()
-#         if isinstance(expected_response_model, Model):
-#             expected_response_model = asdict(expected_response_model)
-#         return True if expected_response_model == response else False
-#
-#
+def validate_headers(headers_to_be_present: dict, response: Response) -> bool:
+    response_headers = response.headers
+    if headers_to_be_present.items() <= response_headers.items():
+        return True
+    else:
+        return False
+
