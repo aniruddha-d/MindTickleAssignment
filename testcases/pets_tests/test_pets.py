c7ad1ab70c093d810be5443e66b8b05cfe17ca7b
Aniruddha Daryapurkar <daryapurkar.aniruddha@gmail.com>
2021-05-09 18:55:14 +0530

    Add test cases

--- testcases/pets_tests/test_pets.py
+++ testcases/pets_tests/test_pets.py
@@ -1,14 +1,28 @@
 import pytest
 import logging
-from actions.users import Users
-from testdata.datafactory.user_factory import ModelUser, ModelUserList
+from actions.pets import Pets
+from testdata.datafactory.pet_factory import ModelPet
+from http import HTTPStatus
+from lib.api.validator import validate_response_body, validate_response_body_as_json, validate_schema
 
-
-@pytest.mark.pet
-def test_create_pets():
-    assert 1 + 1 == 2
-
+from dataclasses import asdict
 
 @pytest.mark.pet
 def test_create_pet():
-    assert 1 + 1 == 2
+    m1 = ModelPet()   # model
+    resp = Pets().create_pet(m1)
+    assert resp.status_code == 200
+
+    resp2 = Pets().get_pet(m1.id)  # response
+    assert validate_response_body(resp2, m1) == True
+    resp2 = resp2.json()
+    assert validate_response_body(resp2, m1) == True
+
+    resp2 = Pets().get_pet(m1.id)  # response
+    assert validate_response_body_as_json(resp2, m1) == True
+    assert validate_response_body_as_json(resp2, asdict(m1)) == True
+
+    resp2 = resp2.json()
+    assert validate_response_body_as_json(resp2, m1) == True
+    assert validate_response_body_as_json(resp2, asdict(m1)) == True
+
--- testcases/users_tests/test_user.py
+++ testcases/users_tests/test_user.py
@@ -1,7 +1,10 @@
 import pytest
 import logging
 from actions.users import Users
-from testdata.datafactory.user_factory import ModelUser, ModelUserList
+from testdata.datafactory.user_factory import ModelUser
+from http import HTTPStatus
+from lib.api.validator import validate_response_body, validate_response_body_as_json, validate_schema
+from dataclasses import asdict
 
 
 @pytest.mark.user
@@ -13,12 +16,25 @@ def test_create_users():
     u4 = ModelUser()
 
     resp = Users().create_users([u1, u2, u3, u4])
-    assert resp.status_code == 200
+    assert resp.status_code == HTTPStatus.OK
     logging.info(resp.status_code)
 
 
 @pytest.mark.user
 def test_create_user():
-    resp = Users().create_user(ModelUser())
+    m1 = ModelUser()  # model
+    resp = Users().create_user(m1)
     assert resp.status_code == 200
-    logging.info(resp.status_code)
+
+    resp2 = Users().get_user(m1.username)  # response
+    assert validate_response_body(resp2, m1) == True
+    resp2 = resp2.json()
+    assert validate_response_body(resp2, m1) == True
+
+    resp2 = Users().get_user(m1.username)  # response
+    assert validate_response_body_as_json(resp2, m1) == True
+    assert validate_response_body_as_json(resp2, asdict(m1)) == True
+
+    resp2 = resp2.json()
+    assert validate_response_body_as_json(resp2, m1) == True
+    assert validate_response_body_as_json(resp2, asdict(m1)) == True
